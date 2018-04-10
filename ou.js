550d639bafd72efd72b9859ffacaff05a5fdec1c
lampolts <37888478+lampolts@users.noreply.github.com>
2018-04-10 00:20:51 -0300

    Create ou.js

---
+++ ou.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+    var ouo_token = 'UUUWcHgO';
+    var exclude_domains = ['hyuugadownloads.com.br/protetor/?id']; 
+</script>
+<script src="//cdn.ouo.io/js/full-page-script.js"></script>
